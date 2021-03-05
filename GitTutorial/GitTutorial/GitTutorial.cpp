df3beaa66e5bac455e35de42b27fdfdcb1dc4a00
ZoeZou9 <zoezou9@gmail.com>
2021-03-05 11:44:45 +0800

    practice8-2

--- GitTutorial/GitTutorial/GitTutorial.cpp
+++ GitTutorial/GitTutorial/GitTutorial.cpp
@@ -1,25 +1,22 @@
-﻿// GitTutorial.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
-#include <iostream>
-// #include "stdafx.h"
+﻿//#include "stdafx.h"
 #include <string>
-
-// using namespace System;
-
-bool Guess(	int number) {
+#include <iostream>
+using namespace System;
+bool Guess(int number) {
 	static int target = -1;
-
 	if (target == -1) {
 		Random r;
 		target = r.Next() % 100 + 1;
 	}
-	if (number == target)
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
 		return true;
+	}
+	else std::cout << "Wrong" << std::endl;
 	return false;
 }
-
-int main()
+int main(array<System::String ^> ^args)
 {
 	int guess;
 	do {
@@ -27,15 +24,3 @@ int main()
 	} while (!Guess(guess));
 	return 0;
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
-
